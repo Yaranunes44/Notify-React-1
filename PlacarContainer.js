c2519c7c0518c51f1df08a0e5d8ac05dc332e500
yaranunes44 <ynunes580@gmail.com>
2019-10-03 17:57:41 -0300

    Placar-fullStack-Project

---
+++ PlacarContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Placar from "./Placar";
+import './PlacarContainer.sccs';
+const PlacarContainer = () => {
+    return <div> className "Placar" </div> 
+}
+
+export default PlacarContainer
